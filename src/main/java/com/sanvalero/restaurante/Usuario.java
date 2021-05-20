33ca66c948c90bac4e05ad3886eec7329b30824c
Miguel Rivero <a25525@svalero.com>
2021-05-20 19:31:18 +0100

    develop

--- src/main/java/com/sanvalero/restaurante/Usuario.java
+++ src/main/java/com/sanvalero/restaurante/Usuario.java
@@ -1,36 +1,5 @@
 package com.sanvalero.restaurante;
 
 public class Usuario{
-        private String nombre;
-        private String apellido;
-        private String email;
-        private String password;
-
-        public String getNombre(){
-            return nombre;
-        }
-        public void setNombre(String nombre){
-            this.nombre = nombre;
-        }
-
-        public String getApellido(){
-            return apellido;
-        }
-        public void setApellido(String apellido){
-                this.apellido = apellido;
-        }
-
-        public String getEmail(){
-           return email;
-        }
-        public void setCorreo(String email){
-            this.email = email;
-        }
         
-        public String getPassword(){
-           return password;
-        }
-        public void setPassword(String password){
-            this.password = password;
-        }
 }
